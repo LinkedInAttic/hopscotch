593bd097515ac040de4b30d077ca8247c4f63ed0
Timothy Lindvall <tlindvall@linkedin.com>
2015-10-20 19:26:50 -0700

    Doclets for Callout classes and CalloutManager

--- src/es/managers/CalloutManager.js
+++ src/es/managers/CalloutManager.js
@@ -4,12 +4,36 @@ import * as Callouts from '../modules/callout.js';
 let globalCfg;
 let callouts = {};
 
-//CalloutManager class definition
-//Handles lyfecycles of standalone callouts
+/**
+ * The CalloutManager handles the lifecycle of
+ * individual callouts, providing the ability to
+ * register, read, and destroy individual callouts.
+ */
 export default class CalloutManager {
+  /**
+   * Constructs the callout manager.
+   *
+   * @param {Config} globalConfig - The global configuration object, as
+   *                                provided by Hopscotch.
+   */
   constructor(globalConfig) {
     globalCfg = globalConfig;
   }
+
+  /**
+   * Create a new individual callout on the page. Similar to tours, you'll
+   * pass in a configuration object with information about the callout's
+   * contents, target, event handlers, and any other additional details.
+   *
+   * Callouts configurations are required to include a unique identifier,
+   * which can later be used to fetch the callout. You'll receive an
+   * error if the ID conflicts with a callout that hasn't yet been removed.
+   *
+   * @param {Object} configHash - The configuration for the callout you're
+   *                              creating. See the docs for details about
+   *                              what configuration options are recognized.
+   * @returns {Callout} The newly created callout, rendered and showing.
+   */
   createCallout(configHash) {
     let callout = this.getCallout(configHash.id);
     if (callout) {
@@ -26,9 +50,23 @@ export default class CalloutManager {
     }
     return callout;
   }
+  
+  /**
+   * Fetch an individual callout instance by its ID.
+   *
+   * @param {String} calloutId - The callout ID to look up.
+   * @returns {?Callout} The callout matching that ID.
+   */
   getCallout(calloutId) {
     return callouts[calloutId];
   }
+
+  /**
+   * Destroys the given callout and removes it from the manager's
+   * registry of active callouts.
+   *
+   * @param {String} calloutId - The callout ID to destroy.
+   */
   removeCallout(calloutId) {
     let callout = this.getCallout(calloutId);
     if (callout) {
@@ -36,6 +74,11 @@ export default class CalloutManager {
       delete callouts[calloutId];
     }
   }
+
+  /**
+   * Destroy all active individual callouts and removes each
+   * from the manager's registry of active callouts.
+   */
   removeAllCallouts() {
     for (let calloutId in callouts) {
       if (callouts.hasOwnProperty(calloutId)) {
