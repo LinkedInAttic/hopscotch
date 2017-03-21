21096dcfba868c9f1dae8a8d8ec20d19cd085ef7
Sebastian Penhouet <spe@itdesign.de>
2017-03-21 16:48:47 +0100

    Calculate arrow offset without border
    #325

--- src/js/hopscotch.js
+++ src/js/hopscotch.js
@@ -665,7 +665,7 @@
       else if (step.placement === 'top' || step.placement === 'bottom') {
         arrowEl.style.top = '';
         if (arrowOffset === 'center') {
-          arrowEl.style[arrowPos] = Math.floor((bubbleBoundingWidth / 2) - arrowEl.offsetWidth/2) + 'px';
+          arrowEl.style[arrowPos] = Math.floor((el.clientWidth / 2) - arrowEl.offsetWidth/2) + 'px';
         }
         else {
           // Numeric pixel value
@@ -675,7 +675,7 @@
       else if (step.placement === 'left' || step.placement === 'right') {
         arrowEl.style[arrowPos] = '';
         if (arrowOffset === 'center') {
-          arrowEl.style.top = Math.floor((bubbleBoundingHeight / 2) - arrowEl.offsetHeight/2) + 'px';
+          arrowEl.style.top = Math.floor((el.clientHeight / 2) - arrowEl.offsetHeight/2) + 'px';
         }
         else {
           // Numeric pixel value
